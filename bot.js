8c3d5cb835a741bdd4d395f54e05bcde3fe754c6
Micah Koga <m@mkoga.com>
2015-08-05 16:44:55 +0000

    Add support for updating schedules

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@ var Slack = require('slack-client');
 var token = 'xoxb-7891720802-LbT8vX5q3ioVDuRvQN81UltY';
 var slack = new Slack(token, true, true);
 
+var scheduled_events = {};
+var event_names = ['Tactics', 'Master analysis', 'Endgame studies'];
+
 var makeMention = function(userId) {
   return '<@' + userId + '>';
 };
@@ -20,6 +23,37 @@ var getOnlineHumansForChannel = function(channel) {
     .map(function(id) { return slack.users[id]; })
     .filter(function(u) { return !!u && !u.is_bot && u.presence === 'active'; });
 };
+
+var parseEventName = function(text) {
+  var result;
+
+  event_names.forEach(function(event_name) {
+    if (text.indexOf(event_name) != -1) {
+      var parts = text.split(event_name);
+      result = [event_name, parts[1]];
+      return;
+    }
+  });
+
+  return result;
+};
+
+var renderSchedule = function(user) {
+  // If unscheduled, use copy "_Unscheduled_. Message makeMention("dcwoods") to coordinate times."
+  // If scheduled, use formating "{days of the week} at {time} {timezone}"
+  // "*Tactics*: Two sessions on Tuesday and Wednesday at 7PM EST & 9PM EST." + "\\r\\n" +
+  // "*Master analysis*: _Unscheduled_. Message " + makeMention("dcwoods") + " to coordinate times." + "\\r\\n" +
+  // "*Endgame studies*: _Unscheduled_. Message " + makeMention("dcwoods") + " to coordinate times.";
+
+  var schedule = makeMention(user.name) + ": Our meeting times for this week are:" + "\r\n\r\n";
+
+  event_names.forEach(function(event_name) {
+    var event_time = scheduled_events[event_name] || "_Unscheduled_. Message " + makeMention("dcwoods") + " to coordinate times.";
+    schedule += "*" + event_name + "*: " + event_time + "\r\n";
+  });
+
+  return schedule;
+}
 
 slack.on('open', function () {
   var channels = Object.keys(slack.channels)
@@ -52,14 +86,37 @@ slack.on('message', function(message) {
   var user = slack.getUserByID(message.user);
 
   if (message.type === 'message' && isDirect(slack.self.id, message.text)) {
-  // If unscheduled, use copy "_Unscheduled_. Message makeMention("dcwoods") to coordinate times."
-  // If scheduled, use formating "{days of the week} at {time} {timezone}"
-    var message = makeMention(user.name) + ": Our meeting times for this week are:" + "\r\n\r\n" + 
-                  "*Tactics*: Two sessions on Tuesday and Wednesday at 7PM EST & 9PM EST." + "\r\n" + 
-                  "*Master analysis*: _Unscheduled_. Message " + makeMention("dcwoods") + " to coordinate times." + "\r\n" + 
-                  "*Endgame studies*: _Unscheduled_. Message " + makeMention("dcwoods") + " to coordinate times.";
+    var parts = message.text.split(" ");
+    var command = (parts[1] || "").toLowerCase();
 
-    channel.send(message);
+    switch (command) {
+      case "clean":
+        var event_parts = parseEventName(message.text);
+
+        if (event_parts) {
+          var event_name = event_parts[0];
+          delete scheduled_events[event_name];
+        } else {
+          scheduled_events = {};
+        }
+
+        channel.send(renderSchedule(user));
+        break;
+
+      default:
+        var event_parts = parseEventName(message.text);
+
+        if (event_parts) {
+          var event_name = event_parts[0];
+          var event_time = event_parts[1].trim();
+
+          if (event_name && event_time) {
+            scheduled_events[event_name] = event_time;
+          }
+        }
+
+        channel.send(renderSchedule(user));
+    }
   }
 });
 
